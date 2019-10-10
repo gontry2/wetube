0c8c5d4002e84a35342b9ff87e09055781c63cd3
choya <gontry2@naver.com>
2019-10-10 23:34:27 +0900

    #Express JS_20191010

---
+++ db.js
@@ -0,0 +1,49 @@
+export const videos = [{
+        id: 11111,
+        title: "Video Awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p)surround.mp4",
+        creator: {
+            id: 12121212,
+            name: "chochonghee",
+            eamil: "chong@gemail.com"
+        }
+    },
+    {
+        id: 22222,
+        title: "Video Good",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p)surround.mp4",
+        creator: {
+            id: 12121212,
+            name: "chochonghee",
+            eamil: "chong@gemail.com"
+        }
+    },
+    {
+        id: 33333,
+        title: "Video Nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p)surround.mp4",
+        creator: {
+            id: 12121212,
+            name: "chochonghee",
+            eamil: "chong@gemail.com"
+        }
+    },
+    {
+        id: 44444,
+        title: "Wonderful Video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p)surround.mp4",
+        creator: {
+            id: 12121212,
+            name: "chochonghee",
+            eamil: "chong@gemail.com"
+        }
+    }
+]
