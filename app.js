7f490c69460a9d910a67e00ac2b0658ec6821e38
choya <gontry2@naver.com>
2019-11-21 19:07:25 +0900

    #7.1 without Facebook Authentication.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
 import express from "express";
+import morgan from "morgan";
 import helmet from "helmet";
-import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
+import "./passport";
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -17,14 +24,21 @@ app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(
-    bodyParser.urlencoded({
-        extended: true
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new CookieStore({ mongooseConnection: mongoose.connection })
     })
 );
-app.use(logger("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
+
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
-
+const ME = "/me";
 
 // Videos
 const VIDEOS = "/videos";
@@ -19,6 +19,11 @@ const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
 
 const routes = {
     home: HOME,
@@ -28,33 +33,28 @@ const routes = {
     search: SEARCH,
     users: USERS,
     userDetail: id => {
-        if (id)
-            return `/users/${id}`;
-        else
-            return USER_DETAIL;
+        if (id) return `/users/${id}`;
+        else return USER_DETAIL;
     },
     editProfile: EDIT_PROFILE,
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
     upload: UPLOAD,
     videoDetail: id => {
-        if (id)
-            return `/videos/${id}`;
-        else
-            return VIDEO_DETAIL;
+        if (id) return `/videos/${id}`;
+        else return VIDEO_DETAIL;
     },
     editVideo: id => {
-        if (id)
-            return `/videos/${id}/edit`;
-        else
-            return EDIT_VIDEO;
+        if (id) return `/videos/${id}/edit`;
+        else return EDIT_VIDEO;
     },
     deleteVideo: id => {
-        if (id)
-            return `/videos/${id}/delete`;
-        else
-            return DELETE_VIDEO;
+        if (id) return `/videos/${id}/delete`;
+        else return DELETE_VIDEO;
     },
+    github: GITHUB,
+    githubCallback: GITHUB_CALLBACK,
+    me: ME
 };
 
 export default routes;
